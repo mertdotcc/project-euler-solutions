9ad6badccb017825e45958c36637af5ad587516b
Mert Öztürk <41954671+mertovski1@users.noreply.github.com>
2019-04-06 14:42:24 +0300

    Update 007.py

--- 007.py
+++ 007.py
@@ -1,20 +1,32 @@
-def isPrime(n):
-    if (n == 1):
-        return False
-    elif (n == 2):
-        return True
-    else:
-        for x in range(2,n):
-            if(n % x == 0):
+import math
+import time
+
+def isPrime(number):
+    if number > 1:
+        if number == 2:
+            return True
+        if number % 2 == 0:
+            return False
+        for current in range(3, int(math.sqrt(number) + 1), 2):
+            if number % current == 0:
                 return False
         return True
-
+    return False
 
 def primeAtIndex(index):
-    counter = 1
-    num = 3
-    while counter != index:
-        if isPrime(num):
-            counter+=1
-        num+=2
-    return num-2
+    start = time.time()
+    if(index == 1):
+        num = 2
+        print("Result:", 2)
+        end = time.time() - start
+        print("Runtime:", end)
+    else:
+        counter = 1
+        num = 3
+        while counter != index:
+            if isPrime(num):
+                counter+=1
+            num+=2
+        print("Result:", num-2)
+        end = time.time() - start
+        print("Runtime:", end)
