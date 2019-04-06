6fbf40623064a87a9d267b9e964d286b388de360
Mert Öztürk <41954671+mertovski1@users.noreply.github.com>
2019-04-06 16:47:14 +0300

    Update 007.py

--- 007.py
+++ 007.py
@@ -2,13 +2,13 @@ import math
 import time
 
 def isPrime(number):
-    if number > 1:
-        if number == 2:
+    if(number > 1):
+        if(number == 2):
             return True
-        if number % 2 == 0:
+        if(number % 2 == 0):
             return False
         for current in range(3, int(math.sqrt(number) + 1), 2):
-            if number % current == 0:
+            if(number % current == 0):
                 return False
         return True
     return False
