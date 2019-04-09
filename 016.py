df1ab869a89adade00ae619d2eb2390b241f7c0c
Mert Öztürk <mert1ozturk@gmail.com>
2019-04-09 10:48:21 +0300

    Update 016.py

--- 016.py
+++ 016.py
@@ -4,6 +4,6 @@ def solve():
     start = time.time()
     num = 2**1000
     s = sum(int(n) for n in str(num))
-    print("Solution:", s)
+    print("Solution:",s)
     end = time.time() - start
-    print("Runtime:", end)
+    print("Runtime:",end)
